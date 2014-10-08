c3ddd4db3bf16d730d87388bce3f7e6c53e40e2f
minix <mgtuiw@gmail.com>
2014-10-09 00:18:00 +0800

    增加一行内容，不过在MAC下使用zsh会segmentation fault

--- add_mac.sh
+++ add_mac.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 usage(){
-        echo 'Usages: $0 NO_:_MACADDRESS'
+        echo 'Usages: $0 NO_COLON_OF_MACADDRESS'
         exit 1
 }
 
@@ -11,3 +11,5 @@ if [ $# == 1 ]; then
 else
         usage
 fi
+
+#[ $# == 1 ] && { arr=($(echo $1 | fold -b2)); echo ${arr[0]}:${arr[1]}:${arr[2]}:${arr[3]}:${arr[4]}:${arr[5]}; } || { usage; }
